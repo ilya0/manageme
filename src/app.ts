dd97c34e8671ff69bacc6519a9d0465a8d0f9d1c
Ilya Osovets <iosovets@gmail.com>
2017-06-01 16:14:34 -0700

    added some pages and setup

--- src/app.ts
+++ src/app.ts
@@ -11,8 +11,8 @@ export class App {
       { route: ['', 'welcome'], name: 'welcome', moduleId: PLATFORM.moduleName('./welcome'), nav: true, title: 'Welcome' },
       { route: 'users', name: 'users', moduleId: PLATFORM.moduleName('./users'), nav: true, title: 'Github Users' },
       { route: 'child-router', name: 'child-router', moduleId: PLATFORM.moduleName('./child-router'), nav: true, title: 'Child Router' },
-      { route: 'instagrampage', name: 'instagram_page', moduleId: PLATFORM.moduleName('./child-router'), nav: true, title: 'Child Router' },
-
+      { route: 'instagrampage', name: 'instagram_page', moduleId: PLATFORM.moduleName('./child-router'), nav: true, title: 'Instagram' },
+      { route: 'instagrampage', name: 'instagram_page', moduleId: PLATFORM.moduleName('./child-router'), nav: true, title: 'Contacts' },
     ]);
 
     this.router = router;
